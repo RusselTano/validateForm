cf9da88e701b8e339bf7910ee48f6484d6f8e71a
Dylane <tanolamene@gmail.com>
2024-07-26 12:45:53 -0300

    refactor

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const usernameInput = document.getElementById("username");
-  const usernameError = document.getElementById("usernameError");
+const usernameInput = document.getElementById("username");
+const usernameError = document.getElementById("usernameError");
 
-  usernameInput.addEventListener("blur", function() {
-      if (usernameInput.value.trim() === "") {
-          usernameError.style.display = "block";
-      } else {
-          usernameError.style.display = "none";
-      }
-  });
+usernameInput.addEventListener("blur", function () {
+  if (usernameInput.value.trim() === "") {
+    usernameError.style.display = "block";
+  } else {
+    usernameError.style.display = "none";
+  }
 });
